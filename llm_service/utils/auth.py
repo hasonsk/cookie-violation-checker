87bb40ed9be5a14d3dec18cbc848096af6918c2e
hasonsk <sonhh2003@gmail.com>
2025-06-27 10:36:43 +0700

    add private api key

--- llm_service/utils/auth.py
+++ llm_service/utils/auth.py
@@ -5,7 +5,7 @@ from fastapi.security.api_key import APIKeyQuery
 
 load_dotenv(find_dotenv())
 
-VALID_API_KEYS = os.environ.get("LLM_API_KEYS", "secret-api-key")
+VALID_API_KEYS = os.environ.get("LLM_API_KEYS")
 
 api_key_query = APIKeyQuery(name="key", auto_error=False)
 
